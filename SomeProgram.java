e6a8cede90b9971df45096bc61e8a1c1eeba95b0
Krystian Zawadzki <kzawadzki@student.agh.edu.pl>
2025-01-18 09:34:54 +0100

    blablabla

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,5 +3,6 @@ package org.example;
 public class SomeProgram {
     public static void main(String[] args) {
         System.out.println("We are learning to use Git.");
+        System.out.println("We will be Git experts, soon.");
     }
 }
